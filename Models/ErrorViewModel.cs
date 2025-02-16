b034cf18a6c0d07d1c33b85c3de6a96016ce5e93
Eriny Adel <e.adel@singleclic.com>
2025-02-16 14:49:03 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LingoCourses.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
